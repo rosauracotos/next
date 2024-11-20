05db7be4387ee8d92fdd64328a333ab775b60c99
rosaura <rosacotossullon@gmail.com>
2024-11-20 04:55:00 -0500

    destino de imagen

--- src/libs/processImage.js
+++ src/libs/processImage.js
@@ -1,11 +1,17 @@
 import path from "path";
-import {writeFile} from "fs/promises";
+import { writeFile } from "fs/promises";
 
-export async function processImage(imagen){
-    const bytes = await imagen.arrayBuffer()
-    const buffer =  Buffer.from(bytes)
+export async function processImage(imagen) {
+    // Convertir el archivo recibido en un buffer
+    const bytes = await imagen.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
-    const filePath =  path.join(process.cwd(), 'public', imagen.name)
-    await writeFile (filePath, buffer)
+    // Cambiar la ruta a /tmp
+    const filePath = path.join("/tmp", imagen.name); // /tmp es permitida en Vercel
+
+    // Escribir el archivo en la ruta temporal
+    await writeFile(filePath, buffer);
+
+    // Devolver la ruta completa al archivo para usarla luego
     return filePath;
 }
